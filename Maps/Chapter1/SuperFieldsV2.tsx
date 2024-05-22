e5c1b054eb6793befb0e7f499c88c17d0e6ce73f
Alec Currie <alecf.currie@gmail.com>
2024-05-21 20:36:26 -0700

    Started Tileset Import Process

--- Maps/Chapter1/SuperFieldsV2.tsx
+++ Maps/Chapter1/SuperFieldsV2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="SuperFieldsV2" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../FE-Repo-Tilesets-Fields/FE8 - Fields - Remaster - Super Fields - Tileset/[A] Preview.png" width="512" height="512"/>
+<tileset version="1.9" tiledversion="1.9.2" name="SuperFieldsV2" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../Tilesets/SuperFields/[A] Preview.png" width="512" height="512"/>
 </tileset>
