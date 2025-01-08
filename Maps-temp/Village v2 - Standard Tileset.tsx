e0558fa1720e63b34bf360c18d415dd72302c3bf
Alec Currie <alecf.currie@gmail.com>
2025-01-07 22:41:43 -0800

    added temp maps

---
+++ Maps-temp/Village v2 - Standard Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Village v2 - Standard Tileset" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../FEMapCreator/Tilesets/Village v2 Tileset {flasuban, N426, ZoramineFae, Venno}/Village v2 - Standard Tileset.png" width="512" height="512"/>
+</tileset>
