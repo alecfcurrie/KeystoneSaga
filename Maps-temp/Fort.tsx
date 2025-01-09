f71b929725b9ecc77ce845ff4f76cdb539d87c65
Alec Currie <55813947+alecfcurrie@users.noreply.github.com>
2025-01-08 19:54:58 -0700

    Removed Temporary Assets

--- Maps-temp/Fort.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Fort" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../Keystone Saga/KeystoneSaga/Maps/Tilesets/Fort/FE8 - Fort - 3c003d3e.png" width="512" height="512"/>
-</tileset>
